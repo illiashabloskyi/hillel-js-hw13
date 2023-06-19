583f8cd95ba76fa7030ccbd5b20a743d94a52b18
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-19 23:20:21 +0300

    update script.js

--- task 03/script.js
+++ task 03/script.js
@@ -2,23 +2,28 @@
 // Довжину основного масиву і внутрішніх масивів задає користувач.
 // Значення всіх елементів всіх масивів задає користувач.
 
-const doubleArray = (m, n) => {
+const doubleArray = (m) => {
   if (
     m === "" ||
     isNaN(m) ||
-    m === null ||
-    n === "" ||
-    isNaN(n) ||
-    n === null
+    m === null
   ) {
     return `Щось пішло не так...`;
   }
   let arr = [];
   for (let i = 0; i < m; i++) {
     arr[i] = [];
-    for (let j = 0; j <= n; j++) {
+    const n = parseInt(prompt(`Введіть довжину внутрішнього масиву ${i + 1}:`));
+    if (
+      n === "" ||
+      isNaN(n) ||
+      n === null
+    ) {
+      return `Щось пішло не так...`;
+    }
+    for (let j = 0; j < n; j++) {
       arr[i][j] = [];
-      let elem = prompt(`Введіть елемент ${j} масиву ${i}`);
+      let elem = prompt(`Введіть елемент ${j + 1} масиву ${i + 1}`);
       arr[i][j].push(elem);
     }
   }
@@ -26,6 +31,5 @@ const doubleArray = (m, n) => {
 };
 
 const m = parseInt(prompt("Введіть довжину основного масиву:"));
-const n = parseInt(prompt("Введіть довжину внутрішнього масиву:"));
 
-console.log(doubleArray(m, n));
+console.log(doubleArray(m));
